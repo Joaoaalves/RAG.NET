19ef5fb3ccfb66998b13af7446c5ee3086143550
Joaoaalves <joaoaa21@hotmail.com>
2025-04-23 20:16:33 -0300

    feat: added findByWorkflowId method to CallbackRepository

--- api/RAGNet.Infrastructure/Repositories/CallbackUrlRepository.cs
+++ api/RAGNet.Infrastructure/Repositories/CallbackUrlRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using RAGNET.Domain.Entities;
 using RAGNET.Domain.Repositories;
 using RAGNET.Infrastructure.Data;
@@ -5,5 +6,19 @@ using RAGNET.Infrastructure.Data;
 namespace RAGNET.Infrastructure.Repositories
 {
     public class CallbackUrlRepository(ApplicationDbContext context) : Repository<CallbackUrl>(context), ICallbackUrlRepository
-    { }
+    {
+        public async Task<List<string>> GetByWorkflowAsync(Guid workflowId)
+        {
+            var result = await _context.CallbackUrls.Where(c => c.WorkflowId == workflowId).ToListAsync();
+
+            List<string> callbackUrls = [];
+
+            foreach (var callbackUrl in result)
+            {
+                callbackUrls.Add(callbackUrl.Url);
+            }
+
+            return callbackUrls;
+        }
+    }
 }
