0316af6271d85655d9fc28e2f3e24f2d972480bb
Joaoaalves <joaoaa21@hotmail.com>
2025-05-05 15:25:16 -0300

    feat: improved jobs bar UI, close job bar on outside click

--- client/src/app/shared/components/jobs/job-bar.component.ts
+++ client/src/app/shared/components/jobs/job-bar.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  Renderer2,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { JobItem } from 'src/app/models/job';
@@ -10,9 +17,31 @@ import { JobComponent } from './job.component';
   imports: [CommonModule, JobComponent],
   templateUrl: './job-bar.component.html',
 })
-export class JobBarComponent {
+export class JobBarComponent implements AfterViewInit, OnDestroy {
   @Input() jobs$!: Observable<JobItem[]>;
   isOpen = false;
+
+  private globalClickListener: (() => void) | undefined;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.globalClickListener = this.renderer.listen(
+      'document',
+      'click',
+      (event: Event) => {
+        if (this.isOpen && !this.elRef.nativeElement.contains(event.target)) {
+          this.isOpen = false;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.globalClickListener) {
+      this.globalClickListener();
+    }
+  }
 
   toggleJobsBar() {
     this.isOpen = !this.isOpen;
