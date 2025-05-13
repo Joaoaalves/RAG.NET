f370bb1aca1cc70d30a183af8690367903723ed8
Joaoaalves <joaoaa21@hotmail.com>
2025-05-13 20:36:15 -0300

    feat: improved about photo animation

--- client/src/app/shared/components/about/about.component.ts
+++ client/src/app/shared/components/about/about.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   standalone: true,
 })
-export class AboutComponent {}
+export class AboutComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.set('#about-card', {
+      y: '-50%',
+      rotationX: 30,
+      z: -100,
+    });
+
+    gsap.to('#about-card', {
+      y: 0,
+      rotationX: 0,
+      z: 0,
+      scrollTrigger: {
+        trigger: '#about-card',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+    });
+  }
+}
