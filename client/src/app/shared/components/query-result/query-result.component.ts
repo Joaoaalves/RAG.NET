b4cadde13503edad9a99aa67b5a54a9495743de6
Joaoaalves <joaoaa21@hotmail.com>
2025-05-23 15:00:34 -0300

    feat: improved query results UI

--- client/src/app/shared/components/query-result/query-result.component.ts
+++ client/src/app/shared/components/query-result/query-result.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideSearch } from '@ng-icons/lucide';
 import { ContentItem } from 'src/app/models/query';
 
 @Component({
   selector: 'app-query-result',
   templateUrl: './query-result.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, NgIcon],
+  providers: [
+    provideIcons({
+      lucideSearch,
+    }),
+  ],
   standalone: true,
   styles: `
     :host{
@@ -13,7 +20,40 @@ import { ContentItem } from 'src/app/models/query';
     }
   `,
 })
-export class QueryResultComponent {
+export class QueryResultComponent implements OnChanges {
+  @Input() workflowId!: string;
   @Input() chunks: ContentItem[] = [];
   @Input() filteredContent: string[] = [];
+
+  currentTab = 0;
+
+  setTab(tab: number) {
+    this.currentTab = tab;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const chunksChanged = !!changes['chunks'];
+    const filteredChanged = !!changes['filteredContent'];
+
+    const prevChunks = changes['chunks']?.previousValue as
+      | ContentItem[]
+      | undefined;
+    const prevFiltered = changes['filteredContent']?.previousValue as
+      | string[]
+      | undefined;
+
+    if (
+      chunksChanged &&
+      (this.filteredContent.length === 0 ||
+        (prevFiltered && prevFiltered.length === this.filteredContent.length))
+    ) {
+      this.currentTab = 1;
+    } else if (
+      filteredChanged &&
+      (this.chunks.length === 0 ||
+        (prevChunks && prevChunks.length === this.chunks.length))
+    ) {
+      this.currentTab = 0;
+    }
+  }
 }
